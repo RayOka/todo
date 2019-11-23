091ecd5314cbe8dc3eb74f70a609efe8b5f482a7
raygituser <DellbaseballPS@gmail.com>
2019-11-23 07:18:16 +0000

    タスク一覧表示機能とフォルダ作成機能を作成
    * タスク一覧表示ではfolder_idからtask一覧を取得
    * フォルダ作成機能ではバリデーションを追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -10,11 +11,20 @@ class TaskController extends Controller
     // index
     public function index (int $id) 
     {
+        // 全てのフォルダを取得する
         $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダのタスクを全取得
+        // $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
 }
