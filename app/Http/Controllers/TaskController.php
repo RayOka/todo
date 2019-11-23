5a7acccc8221fca69ed45ce3019783a876c24bcb
raygituser <DellbaseballPS@gmail.com>
2019-11-23 15:38:51 +0000

    タスク作成機能の実装 #2
    * パーッミションの設定やSGIDの設定も行なった

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
-    // index
     public function index (int $id) 
     {
         // 全てのフォルダを取得する
@@ -25,6 +25,34 @@ class TaskController extends Controller
             'folders' => $folders,
             'current_folder_id' => $id,
             'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * GET /folders/{id}/tasks/create
+     */
+    public function showCreateForm (int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id,
+        ]);
+    }
+
+    /**
+     * POST /folders/{id}/tasks/create
+     */
+    public function create (int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder,
         ]);
     }
 }
